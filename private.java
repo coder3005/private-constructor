12e914d9fcf1ede14d25d33c008a026b1cd8266f
coder3005 <138576028+coder3005@users.noreply.github.com>
2023-07-04 15:26:33 +0530

    Add files via upload

---
+++ private.java
@@ -0,0 +1,13 @@
+/* Private Constructor */
+class Y
+{
+    int a; double b; String c;
+    private Y()
+    {
+        a=10; b=30.56; c="abhi";
+        System.out.println(a+" "+b+" "+c);
+    }
+    public static void main(String[] args) {
+        Y r=new Y();
+    }
+}
